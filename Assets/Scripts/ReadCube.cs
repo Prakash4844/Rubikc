6d268ca054eaad28a3567c03809b6777245f961f
Prakash4844 <pk484442@gmail.com>
2023-01-18 04:30:34 +0530

    1. Added Cubemap and Raycast for Each Cube Side(Faces) 2. Scaled Cube Prefab to be 1.1 size and added raycast that goes yellow when cube is hit and green otherwise

--- Assets/Scripts/ReadCube.cs
+++ Assets/Scripts/ReadCube.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class ReadCube : MonoBehaviour
 {
-    public Transform tup;
-    public Transform tdown;
-    public Transform tleft;
-    public Transform tright;
-    public Transform tfront;
-    public Transform tback;
+    public Transform tUp;
+    public Transform tDown;
+    public Transform tLeft;
+    public Transform tRight;
+    public Transform tFront;
+    public Transform tBack;
+
+    private int layerMask = 1 << 8; //This layermask is for the faces of the cube only
+
+    CubeState cubestate;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cubestate = FindObjectOfType<CubeState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        List<GameObject> facesHit= new List<GameObject>();
+
+        Vector3 ray = tFront.transform.position;
+        RaycastHit hit;
+
+        // Does the ray intersect any object in the layermask?
+
+        if(Physics.Raycast(ray, tFront.right, out hit, Mathf.Infinity,layerMask))
+        {
+            Debug.DrawRay(ray, tFront.right * hit.distance, Color.yellow);
+            facesHit.Add(hit.collider.gameObject);
+            print(hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.DrawRay(ray, tFront.right * 1000, Color.green);
+        }
+        cubestate.front = facesHit;
     }
 }
